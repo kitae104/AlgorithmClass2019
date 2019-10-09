78b98c2a167191e59d4205c97ba84933758eca6e
kitae <aqua0405@gmail.com>
2019-10-09 13:47:17 +0900

    ListNode 클래스 수정

--- src/main/java/kr/inhatc/algorithm/ch03/ListNode.java
+++ src/main/java/kr/inhatc/algorithm/ch03/ListNode.java
@@ -5,21 +5,36 @@ public class ListNode {
 	public Object data;
 	public ListNode next;
 
+	/**
+	 * 생성자
+	 */
 	public ListNode() {
 		this.data = null;
 		this.next = null;
 	}
 
+	/**
+	 * 생성자
+	 * @param data
+	 */
 	public ListNode(Object data) {
 		this.data = data;
 		this.next = null;
 	}
 
+	/**
+	 * 생성자
+	 * @param data
+	 * @param next
+	 */
 	public ListNode(Object data, ListNode next) {
 		this.data = data;
 		this.next = next;
 	}
 
+	/**
+	 * 내용 출력
+	 */
 	public String toString() {
 		return "ListNode(" + data.toString() + ")";
 	}
