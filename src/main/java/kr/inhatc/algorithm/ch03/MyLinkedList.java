f9432bfce8f2b1fda5b4250cd12d4c48c8aefe79
kitae <aqua0405@gmail.com>
2019-10-09 09:49:31 +0900

    MyLinkedList 클래스

--- src/main/java/kr/inhatc/algorithm/ch03/MyLinkedList.java
+++ src/main/java/kr/inhatc/algorithm/ch03/MyLinkedList.java
@@ -9,33 +9,41 @@ import java.util.ListIterator;
 public class MyLinkedList<E> implements List<E> {
 
 	/**
-	 * Node is identical to ListNode from the example, 
-	 * but parameterized with T
-	 * 
+	 * Node를 관리하는 클래스  
 	 */
 	private class Node { 
 		public E data;
 		public Node next;
 
+		/**
+		 * 생성자 
+		 * @param data
+		 */
 		public Node(E data) {
 			this.data = data;
 			this.next = null;
 		}
-		@SuppressWarnings("unused")
+				
+		/**
+		 * 생성자 
+		 * @param data
+		 * @param next
+		 */
 		public Node(E data, Node next) {
 			this.data = data;
 			this.next = next;
 		}
+		
 		public String toString() {
 			return "Node(" + data.toString() + ")";
 		}
 	}
 
-	private int size;            // 요소의 개수 관리 
+	private int size;            // 요소의 개수 관리 - 상수시간 관리 
 	private Node head;           // 첫번째 노드 참조
 
 	/**
-	 *
+	 * 생성자
 	 */
 	public MyLinkedList() {
 		head = null;
@@ -53,17 +61,21 @@ public class MyLinkedList<E> implements List<E> {
 		mll.add(3);
 		System.out.println(Arrays.toString(mll.toArray()) + " size = " + mll.size());
 
-		mll.remove(new Integer(2));
+//		mll.remove(new Integer(2));
+		mll.remove(Integer.valueOf(2));
 		System.out.println(Arrays.toString(mll.toArray()) + " size = " + mll.size());
 	}
 
+	/**
+	 * 요소 추가하기 - 마지막에 노드 추가
+	 */
 	@Override
 	public boolean add(E element) {
 		if (head == null) {
 			head = new Node(element);
 		} else {
 			Node node = head;
-			// loop until the last node
+			// 마지막 노드까지 반복 
 			for ( ; node.next != null; node = node.next) {}
 			node.next = new Node(element);	// 마지막에 새로운 노드 추가 
 		}
@@ -71,17 +83,6 @@ public class MyLinkedList<E> implements List<E> {
 		return true;
 	}
 
-	@Override
-	public void add(int index, E element) {
-		//TODO: FILL THIS IN!		
-		if(index == 0) {
-			head = new Node(element, head);
-		} else {
-			Node node = getNode(index - 1);		// 범위에 대한 처리 수행 
-			node.next = new Node(element, node.next);
-		}
-		size++;		
-	}
 
 	@Override
 	public boolean addAll(Collection<? extends E> collection) {
@@ -118,13 +119,17 @@ public class MyLinkedList<E> implements List<E> {
 		return true;
 	}
 
+	/**
+	 * 인덱스에 해당하는 노드의 데이터 반환 
+	 */
 	@Override
 	public E get(int index) {
 		Node node = getNode(index);
 		return node.data;
 	}
 
-	/** Returns the node at the given index.
+	/** 
+	 * 인덱스에 해당하는 노드 반환 
 	 * @param index
 	 * @return
 	 */
@@ -139,18 +144,7 @@ public class MyLinkedList<E> implements List<E> {
 		return node;
 	}
 
-	@Override
-	public int indexOf(Object target) {
-		//TODO: FILL THIS IN!
-		//Node node = head;
-		for (int i = 0; i < size; i++) {
-			if(equals(target, get(i))) {	// 아래 구현된 equals 사용 
-				return i;
-			}
-			//node = node.next;
-		}				
-		return -1;
-	}
+	
 
 	/** Checks whether an element of the array is the target.
 	 *
@@ -211,24 +205,7 @@ public class MyLinkedList<E> implements List<E> {
 		return true;
 	}
 
-	/**
-	 * 인덱스에 해당하는 내용물을 반환하고 해당 위치를 제거한다. 
-	 */
-	@Override
-	public E remove(int index) {
-		//TODO: FILL THIS IN!
-		E element = get(index);		// 인덱스에 해당하는 원소 반환
-		if(index == 0) {
-			head = head.next;			
-		} else {
-			Node node = getNode(index - 1);
-			node.next = node.next.next; 
-		}
-		size--;
-		
-		return element;
-	}
-
+	
 	@Override
 	public boolean removeAll(Collection<?> collection) {
 		boolean flag = true;
@@ -289,4 +266,58 @@ public class MyLinkedList<E> implements List<E> {
 	public <T> T[] toArray(T[] a) {
 		throw new UnsupportedOperationException();
 	}
+	
+	//======================================================
+	// 수정이 필요한 부분(2019.10.09) 
+	//======================================================
+	
+	/**
+	 * 타겟 데이터와 동등한 데이터를 갖고 있는 노드의 위치 반환 
+	 */
+	@Override
+	public int indexOf(Object target) {
+		//TODO: FILL THIS IN!
+		//Node node = head;
+		for (int i = 0; i < size; i++) {
+			if(equals(target, get(i))) {	// 아래 구현된 equals 사용 
+				return i;	// 해당 위치 반환 
+			}
+			//node = node.next;
+		}				
+		return -1;
+	}
+	
+	/**
+	 * 특정한 위치에 노드 추가하기 
+	 */
+	@Override
+	public void add(int index, E element) {
+		//TODO: FILL THIS IN!		
+		if(index == 0) {
+			head = new Node(element, head);
+		} else {
+			Node node = getNode(index - 1);		// 범위에 대한 처리 수행 
+			node.next = new Node(element, node.next);	// 노드를 생성하여 추가
+		}
+		size++;		// 노드 추가후 크기 변경 
+	}
+	
+	/**
+	 * 인덱스에 해당하는 내용물을 반환하고 해당 위치를 제거한다. 
+	 */
+	@Override
+	public E remove(int index) {
+		//TODO: FILL THIS IN!
+		E element = get(index);		// 인덱스에 해당하는 원소 반환
+		if(index == 0) {
+			head = head.next;			
+		} else {
+			Node node = getNode(index - 1);
+			node.next = node.next.next; 
+		}
+		size--;	// 전체 크기를 하나 줄인다. 
+		
+		return element;		// 제거된 값 반환 
+	}
+
 }
